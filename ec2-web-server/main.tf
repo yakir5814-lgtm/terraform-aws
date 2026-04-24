4c004c81c67a0fc02fa03e49296e47232f9b961f
yakir <yakir5814@gmail.com>
2026-04-24 11:00:35 +0300

    Rename public subnet tag from 'task2' to 'server01'

--- ec2-web-server/main.tf
+++ ec2-web-server/main.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_subnet" {
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${var.aws_region}a"
-  tags = { Name = "task2-public-subnet" }
+  tags = { Name = "server01-public-subnet" }
 }
 
 # 3. Create an Internet Gateway
